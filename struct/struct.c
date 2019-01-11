2d331c2f8ced0dd8a669ec4cf5176828cd915cd6
unknown <buvanesh996@gmail.com>
2019-01-11 17:37:26 +0530

    https://github.com/buvabuvi008/struct.git
    git commit -m

---
+++ struct/struct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct Student{
+	int roll;
+	int name[20];
+	char name[20];
+	double gp;
+}
+int main()
+{
+struct Student s1,s2,s3;
+s1.roll=5;
+s2.roll=10;
+
+s1.gp=9.6;	
+return 0;
+}
